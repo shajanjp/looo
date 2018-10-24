cb741ad6b245734a9476eebdaf5e0bb3f1aa9f25
Shajan Jacob <shajanjp@gmail.com>
2018-10-24 17:03:20 +0530

    adds support for color logs and db storage

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const config = require('./sample-config.json');
+const loki = require('lokijs');
+const db = new loki('looo.json');
+const logs = db.addCollection('logs');
 
 function getColor(level){
   switch(level) {
@@ -20,37 +23,36 @@ function getColor(level){
 }
 
 let looo = {
+  db: logs,
+
   log: (...data) => {
     if(config.options.log.console)
       console.log(...data);
     if(config.options.log.db)
-      console.log('logging log to db');
-
+      logs.insert( { level : 'log', data: data } );
   },
 
   info: (...data) => {
     if(config.options.info.console)
-      console.info(getColor("info"), ...data);
+      console.info(getColor("info"), ...data, "\x1b[0m");
     if(config.options.info.db)
-      console.log('logging info to db');
-
+      logs.insert( { level : 'info', data: data } );
   },
 
   warn: (...data) => {
     if(config.options.warn.console)
-      console.warn(getColor("warn"), ...data);
+      console.warn(getColor("warn"), ...data, "\x1b[0m");
     if(config.options.warn.db)
-      console.log('logging warn to db');
-
+      logs.insert( { level : 'warn', data: data } );
   },
 
   error: (...data) => {
     if(config.options.error.console)
-      console.error(getColor("error"), ...data);
+      console.error(getColor("error"), ...data, "\x1b[0m");
     if(config.options.error.db)
-      console.log('logging error to db');
+      logs.insert( { level : 'error', data: data } );
   }
 }
 
 global.looo = looo;
-module.exports = looo;
+module.exports = looo
