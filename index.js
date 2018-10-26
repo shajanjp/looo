046c188ef6e1ff8c0ccef4859f0da6e17dd05c86
Shajan Jacob <shajanjp@gmail.com>
2018-10-26 15:31:30 +0530

    fixes date format and color functions

--- index.js
+++ index.js
@@ -2,28 +2,27 @@ const config = require('./sample-config.json');
 const loki = require('lokijs');
 const db = new loki('looo.json');
 const logs = db.addCollection('logs');
+const helpers = require('./lib/helpers.js');
+const ejs = require("ejs");
 
-function getColor(level){
-  switch(level) {
-    case "error":
-    case 1:
-    return "\x1b[31m";
-
-    case "warn":
-    case 2: 
-    return "\x1b[33m";
-    
-    case "info":
-    case 3: 
-    return "\x1b[32m";
-    
-    default: 
-    return "";
-  }
-}
+let  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 let looo = {
   db: logs,
+  express: (expressApp) => {
+    expressApp.set('view engine', 'ejs');
+    expressApp.get('/looo', (req, res) => {
+      let logsData = logs.find({});
+      let logsDataWithDate = logsData.map((log) => {
+        let dateFormat = new Date(log.meta.created); 
+        dateFormat = `${months[dateFormat.getMonth()]} ${dateFormat.getDate()} - ${dateFormat.getHours()}:${dateFormat.getMinutes()}`;
+         log.created = dateFormat;
+         console.log('log', log);
+         return log;
+      })
+      res.render('list', { logs: logs.find({}) });
+    });
+  },
 
   log: (...data) => {
     if(config.options.log.console)
@@ -34,21 +33,21 @@ let looo = {
 
   info: (...data) => {
     if(config.options.info.console)
-      console.info(getColor("info"), ...data, "\x1b[0m");
+      console.info(helpers.getColor("info"), ...data, "\x1b[0m");
     if(config.options.info.db)
       logs.insert( { level : 'info', data: data } );
   },
 
   warn: (...data) => {
     if(config.options.warn.console)
-      console.warn(getColor("warn"), ...data, "\x1b[0m");
+      console.warn(helpers.getColor("warn"), ...data, "\x1b[0m");
     if(config.options.warn.db)
       logs.insert( { level : 'warn', data: data } );
   },
 
   error: (...data) => {
     if(config.options.error.console)
-      console.error(getColor("error"), ...data, "\x1b[0m");
+      console.error(helpers.getColor("error"), ...data, "\x1b[0m");
     if(config.options.error.db)
       logs.insert( { level : 'error', data: data } );
   }
